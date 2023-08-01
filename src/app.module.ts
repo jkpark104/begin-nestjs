5d87b7909ae7f719edd02e6577afc597e596289d
jkpark104 <jkrang104@gmail.com>
2023-08-01 22:36:07 +0900

    feat: docker packaging

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,21 +4,23 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './apis/boards/boards.module';
 import Board from './apis/boards/entities/board.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     BoardsModule,
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'src/common/graphql/schema.gql',
     }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '1234',
-      database: 'myproject',
+      type: process.env.DATABASE_TYPE as 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE,
       entities: [Board],
       synchronize: true,
       logging: true,
