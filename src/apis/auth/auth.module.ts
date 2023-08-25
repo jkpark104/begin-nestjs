dd08b46d77604b3c2a27e23e37b959b9ce281b37
jkpark104 <jkrang104@gmail.com>
2023-08-26 08:34:53 +0900

    feat: 11-03 회원 조회 API - 인가

--- src/apis/auth/auth.module.ts
+++ src/apis/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from '../users/users.module';
 import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
+import { JwtAccessStrategy } from './strategies/jwt-access.strategy';
 
 @Module({
   imports: [JwtModule.register({}), UsersModule], // 모듈을 통해 불러오면 레포지토리를 공유할 수 있다.
-  providers: [AuthResolver, AuthService],
+  providers: [JwtAccessStrategy, AuthResolver, AuthService],
 })
 export class AuthModule {}
