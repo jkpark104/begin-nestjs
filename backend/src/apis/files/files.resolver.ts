5a858b6c01c5464a6d5fae52b56002ae496bdb64
jkpark104 <jkrang104@gmail.com>
2023-10-15 15:09:16 +0900

    feat: file-upload-multiple-promise-all

--- backend/src/apis/files/files.resolver.ts
+++ backend/src/apis/files/files.resolver.ts
@@ -6,11 +6,11 @@ import { FilesService } from './files.service';
 export class FilesResolver {
   constructor(private readonly filesService: FilesService) {}
 
-  @Mutation(() => String)
+  @Mutation(() => [String])
   uploadFile(
-    @Args({ name: 'file', type: () => GraphQLUpload }) file: FileUpload, // GraphQL에서 파일 받아옴
+    @Args({ name: 'files', type: () => [GraphQLUpload] }) files: FileUpload[], // GraphQL에서 파일 받아옴
   ): // 브라우저에서 파일 받아옴
-  Promise<string> {
-    return this.filesService.upload({ file });
+  Promise<string[]> {
+    return this.filesService.upload({ files });
   }
 }
--- backend/src/apis/files/files.service.ts
+++ backend/src/apis/files/files.service.ts
@@ -3,39 +3,45 @@ import { Injectable } from '@nestjs/common';
 import { FileUpload } from 'graphql-upload';
 
 interface IFilesServiceUpload {
-  file: FileUpload;
+  files: FileUpload[];
 }
 
 @Injectable()
 export class FilesService {
-  async upload({ file }: IFilesServiceUpload): Promise<string> {
+  async upload({ files }: IFilesServiceUpload): Promise<string[]> {
     // 1. 파일을 클라우드 스토리지에 업로드하는 로직
 
     // 1-1) 스토리지 셋팅하기
+    const bucket = 'bucket-name';
     const storage = new Storage({
       projectId: 'project-id',
       keyFilename: 'gcp-file-storage.json',
-    }).bucket('bucket-name');
+    }).bucket(bucket);
 
     // 1-2) 스토리지에 파일 업로드하기
-    await new Promise((resolve, reject) => {
-      file.createReadStream().pipe(
-        storage
-          .file(file.filename)
-          .createWriteStream()
-          .on('finish', () => {
-            console.log('성공');
-            resolve('성공');
-          })
-          .on('error', () => {
-            console.log('실패');
-            reject('실패');
+    const results = await Promise.all(
+      files.map(
+        (file) =>
+          new Promise<string>((resolve, reject) => {
+            file.createReadStream().pipe(
+              storage
+                .file(file.filename)
+                .createWriteStream()
+                .on('finish', () => {
+                  console.log('성공');
+                  resolve(`${bucket}/${file.filename}`);
+                })
+                .on('error', () => {
+                  console.log('실패');
+                  reject('실패');
+                }),
+            );
           }),
-      );
-    });
+      ),
+    );
 
     console.log('파일 전송이 완료되었습니다.');
 
-    return '끝';
+    return results;
   }
 }
